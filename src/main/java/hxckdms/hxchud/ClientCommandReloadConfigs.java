a6b54dc3af7acea2d64db2d6af91668d33b72794
drzed <kyahne12@yahoo.com>
2018-04-22 12:41:38 -0400

    ver bump

--- src/main/java/hxckdms/hxchud/ClientCommandReloadConfigs.java
+++ src/main/java/hxckdms/hxchud/ClientCommandReloadConfigs.java
@@ -8,6 +8,11 @@ import net.minecraft.server.MinecraftServer;
 
 public class ClientCommandReloadConfigs extends CommandBase {
     @Override
+    public int getRequiredPermissionLevel() {
+        return 0;
+    }
+
+    @Override
     public String getName() {
         return "hxchudreloadconfigs";
     }
