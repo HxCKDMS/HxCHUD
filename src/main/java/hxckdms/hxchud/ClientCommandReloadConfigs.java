57c3de1ddfbc2d6489b191c25e66058244b9e1e8
drzed <kyahne12@yahoo.com>
2018-04-22 13:08:06 -0400

    ALMIGHTY PUSH

--- src/main/java/hxckdms/hxchud/ClientCommandReloadConfigs.java
+++ src/main/java/hxckdms/hxchud/ClientCommandReloadConfigs.java
@@ -2,12 +2,20 @@ package hxckdms.hxchud;
 
 import hxckdms.hxchud.libraries.GlobalVariables;
 import net.minecraft.command.CommandBase;
+import net.minecraft.command.CommandException;
 import net.minecraft.command.ICommandSender;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.util.text.TextComponentString;
 
 public class ClientCommandReloadConfigs extends CommandBase {
     @Override
+    public int getRequiredPermissionLevel() {
+        return 0;
+    }
+
+    @Override
     public String getCommandName() {
-        return "hxchpreloadconfigs";
+        return "hxchudreloadconfigs";
     }
 
     @Override
@@ -16,7 +24,8 @@ public class ClientCommandReloadConfigs extends CommandBase {
     }
 
     @Override
-    public void processCommand(ICommandSender sender, String[] args) {
+    public void execute(MinecraftServer server, ICommandSender sender, String[] args) throws CommandException {
         GlobalVariables.mainConfig.initConfiguration();
+        sender.addChatMessage(new TextComponentString("Successfully reloaded configs."));
     }
 }
