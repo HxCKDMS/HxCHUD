acf79755093031e38a1a9f8d02b212b5abd0399b
drzed <kyahne12@yahoo.com>
2018-04-22 13:09:38 -0400

    added jar added reply for command

--- src/main/java/hxckdms/hxchud/ClientCommandReloadConfigs.java
+++ src/main/java/hxckdms/hxchud/ClientCommandReloadConfigs.java
@@ -5,6 +5,7 @@ import net.minecraft.command.CommandBase;
 import net.minecraft.command.CommandException;
 import net.minecraft.command.ICommandSender;
 import net.minecraft.server.MinecraftServer;
+import net.minecraft.util.text.TextComponentString;
 
 public class ClientCommandReloadConfigs extends CommandBase {
     @Override
@@ -20,5 +21,6 @@ public class ClientCommandReloadConfigs extends CommandBase {
     @Override
     public void execute(MinecraftServer server, ICommandSender sender, String[] args) throws CommandException {
         GlobalVariables.mainConfig.initConfiguration();
+        sender.sendMessage(new TextComponentString("Successfully reloaded configs."));
     }
 }
